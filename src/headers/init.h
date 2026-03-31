67d6139105c4164fe3ef5fadbed7eab832d97bde
dev-marieclaire <marieclaireisabelle@tutamail.com>
2026-03-30 23:33:16 -0600

    modified:   src/headers/defs.h 	modified:   src/headers/init.h 	modified:   src/init.c 	src/headers/init_renderer.h 	src/headers/init_window.h 	src/images.c 	src/init_renderer.c 	src/init_widow.c

--- src/headers/init.h
+++ src/headers/init.h
@@ -1,10 +1,4 @@
 #include <SDL2/SDL.h>
 
-/* UNUSED, for the moment...*/
-extern SDL_Window *init_window();
-extern SDL_Window *init_custom_window(const char *title, uint x, uint y, uint w, uint h, uint flags);
-extern SDL_Renderer *init_renderer(SDL_Window *w);
-extern SDL_Renderer *init_custom_renderer(SDL_Window *w, int index, uint flags);
-
 // extern void init_all();
 extern void init_all(SDL_Window **w, SDL_Renderer **r);
