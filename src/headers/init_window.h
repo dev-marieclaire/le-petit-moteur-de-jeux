0e4c68641ad4bd5372f0292b44ea82a2a10e2b4c
dev-marieclaire <mail.maryjamesb@gmail.com>
2026-04-14 00:09:33 -0600

    Changes to be committed: 	deleted:    src/headers/defs.h 	deleted:    src/headers/img.h 	deleted:    src/headers/init.h 	deleted:    src/headers/init_renderer.h 	deleted:    src/headers/init_window.h 	deleted:    src/headers/input.h 	deleted:    src/headers/main.h 	deleted:    src/headers/screen.h

--- src/headers/init_window.h
+++
@@ -1,4 +0,0 @@
-#include <SDL2/SDL.h>
-
-extern SDL_Window *init_window();
-extern SDL_Window *init_custom_window(const char *title, uint x, uint y, uint w, uint h, uint flags);
--- src/headers/main.h
+++
@@ -1,4 +0,0 @@
-#include "defs.h"
-
-extern game_t game;
-extern game_t *game_ptr;
